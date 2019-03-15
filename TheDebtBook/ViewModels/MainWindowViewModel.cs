06be5b7f67ec48ec9d1261b97c837748217e027a
Maiken Guldberg <maiken.g@fibermail.dk>
2019-03-15 08:47:15 +0100

    Changes to MainWindowViewModel

--- TheDebtBook/ViewModels/MainWindowViewModel.cs
+++ TheDebtBook/ViewModels/MainWindowViewModel.cs
@@ -36,33 +36,15 @@ namespace TheDebtBook
 
 
 
-        public ObservableCollection<DebitorNameTotalDept> DebitorsNameTotalDepts
+        public ObservableCollection<Debitor> Debitors
         {
             get
             {
-                ObservableCollection<DebitorNameTotalDept> list = new ObservableCollection<DebitorNameTotalDept>();
-                foreach (var d in _model.Debitors)
-                {
-                    DebitorNameTotalDept debitorNameTotal = new DebitorNameTotalDept();
-                    debitorNameTotal.Name = d.Name;
-                    debitorNameTotal.TotalDept = GetTotalDept(d);
-                    list.Add(debitorNameTotal);
-                }
-
-                return list;
+               return _model.Debitors;
             }
         }
 
-        private double GetTotalDept(Debitor d)
-        {
-            double totalDept = 0;
-            foreach (var debt in d.Debts)
-            {
-                totalDept = totalDept + debt.Value;
-            }
-
-            return totalDept;
-        }
+        
         #region Commands
         ICommand _addDeptToDebitorCommand;
         private ICommand _addDepitorCommand;
@@ -103,11 +85,7 @@ namespace TheDebtBook
         #endregion
     }
 
-    public class DebitorNameTotalDept
-    {
-        public string Name { get; set; }
-        public double TotalDept { get; set; }
-    }
+    
 
 
 }
