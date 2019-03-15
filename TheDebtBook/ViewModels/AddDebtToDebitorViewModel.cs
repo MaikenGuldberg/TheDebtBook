3fe0d68caafcaa232b209ee9e7216fb804f7413b
Maiken Guldberg <maiken.g@fibermail.dk>
2019-03-15 10:56:15 +0100

    hh

--- TheDebtBook/ViewModels/AddDebtToDebitorViewModel.cs
+++ TheDebtBook/ViewModels/AddDebtToDebitorViewModel.cs
@@ -1,36 +1,72 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Prism.Commands;
+using TheDebtBook.Annotations;
 
 namespace TheDebtBook
 {
-    public class AddDebtToDebitorViewModel
+    public class AddDebtToDebitorViewModel:INotifyPropertyChanged
     {
         private DebtBookModel _model;
+        private double _newDebt = 0;
         public AddDebtToDebitorViewModel(DebtBookModel model)
         {
             _model = model;
         }
 
-        public ObservableCollection<Debt> DebtsDatesValues
+        public double NewDebt
+        {
+            get => _newDebt;
+            set
+            {
+                if (value != _newDebt)
+                {
+                    _newDebt = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<Debt> Debts
         {
             get
             {
-                ObservableCollection<DebtDateValue> list = new ObservableCollection<DebtDateValue>();
-                foreach (var debt in _model.Debitors[_model.CurrentIndex].Debts)
-                {
-                    DebtDateValue debtDateValue = new DebtDateValue();
-                    debtDateValue.Date = debt.Date.ToString("MM/dd/yyyy");
-                    debtDateValue.Value = debt.Value;
-                }
-
                 return _model.Debitors[_model.CurrentIndex].Debts;
             }
         }
-    }
 
-    public class DebtDateValue
-    {
-        public string Date { get; set; }
-        public double Value { get; set; }
+        public string DebitorName
+        {
+            get { return _model.Debitors[_model.CurrentIndex].Name; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region Commands
+
+        public ICommand _addDebtCommand;
+
+        public ICommand AddDebtCommand
+        {
+            get { return _addDebtCommand ?? (_addDebtCommand = new DelegateCommand(AddDebt)); }
+        }
+
+        private void AddDebt()
+        {
+            Debt debt = new Debt(_newDebt,DateTime.Now);
+            _model.AddDeptToDebitor(debt,_model.CurrentIndex);
+        }
+
+
+        #endregion
     }
 }
